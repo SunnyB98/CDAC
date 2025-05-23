8d523473670b205615f60b9523b2c185e05de783
SunnyB98 <113575525+SunnyB98@users.noreply.github.com>
2025-05-23 19:23:59 +0530

    Update Program.cs

--- MS.net/UDP/Server/Program.cs
+++ MS.net/UDP/Server/Program.cs
@@ -2,22 +2,35 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Serilog;
 
-class UdpServer
+class Program
 {
     static void Main()
     {
+        // Configure Serilog
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/server_log.txt", rollingInterval: RollingInterval.Day)
+            // Optional: Send logs over UDP
+            //.WriteTo.Udp("127.0.0.1", 7071)
+            .CreateLogger();
+
         int listenPort = 11000;
         using (UdpClient udpServer = new UdpClient(listenPort))
         {
-            Console.WriteLine($"Server is listening on port {listenPort}...");
+            Log.Information("Server is listening on port {Port}", listenPort);
             IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
             while (true)
             {
                 byte[] receivedBytes = udpServer.Receive(ref remoteEndPoint);
                 string receivedData = Encoding.UTF8.GetString(receivedBytes);
-                Console.WriteLine($"Received from {remoteEndPoint}: {receivedData}");
+                Log.Information("Received from {RemoteEndPoint}: {Data}", remoteEndPoint, receivedData);
             }
         }
+
+        // Ensure to flush and close the logger
+        Log.CloseAndFlush();
     }
 }
