c5ad0937471032fdbe05c2570e2ef403e28815cb
SunnyB98 <113575525+SunnyB98@users.noreply.github.com>
2025-05-23 14:37:13 +0530

    Update Program.cs

--- MS.net/TcpMessengerSolution/TcpServer/Program.cs
+++ MS.net/TcpMessengerSolution/TcpServer/Program.cs
@@ -3,40 +3,44 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Serilog;
 
 class TcpServer
 {
     static void Main()
     {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("Logs/server_log.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        Log.Information("Starting TCP Server...");
+
         TcpListener server = new TcpListener(IPAddress.Any, 5000);
         server.Start();
-        Console.WriteLine("TCP Server started. Waiting for client...");
+        Log.Information("Server started. Waiting for connection...");
 
         TcpClient client = server.AcceptTcpClient();
-        Console.WriteLine("Client connected!");
+        Log.Information("Client connected.");
 
         NetworkStream stream = client.GetStream();
         StreamReader reader = new StreamReader(stream, Encoding.UTF8);
         StreamWriter writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
 
-        using (StreamWriter log = new StreamWriter("server_chatlog.txt", true))
+        while (true)
         {
-            while (true)
-            {
-                string clientMessage = reader.ReadLine();
-                if (clientMessage == null) break;
+            string clientMessage = reader.ReadLine();
+            if (clientMessage == null) break;
 
-                Console.WriteLine("Client: " + clientMessage);
-                log.WriteLine($"Client: {clientMessage}");
-
-                Console.Write("You: ");
-                string reply = Console.ReadLine();
-                writer.WriteLine(reply);
-                log.WriteLine($"Server: {reply}");
-            }
+            Log.Information("Client: {ClientMessage}", clientMessage);
+            Console.Write("You: ");
+            string reply = Console.ReadLine();
+            writer.WriteLine(reply);
+            Log.Information("Server: {Reply}", reply);
         }
 
         client.Close();
         server.Stop();
+        Log.Information("Server stopped.");
     }
 }
