0175595c1eb55f597764b69def4ffc8df04aaf42
SunnyB98 <113575525+SunnyB98@users.noreply.github.com>
2025-05-23 14:02:02 +0530

    Create Program.cs

---
+++ MS.net/TcpMessengerSolution/TcpServer/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpServer
+{
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 5000);
+        server.Start();
+        Console.WriteLine("TCP Server started. Waiting for client...");
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Client connected!");
+
+        NetworkStream stream = client.GetStream();
+        StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+        StreamWriter writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+
+        while (true)
+        {
+            string clientMessage = reader.ReadLine();
+            if (clientMessage == null) break;
+            Console.WriteLine("Client: " + clientMessage);
+
+            Console.Write("You: ");
+            string reply = Console.ReadLine();
+            writer.WriteLine(reply);
+        }
+
+        client.Close();
+        server.Stop();
+    }
+}
