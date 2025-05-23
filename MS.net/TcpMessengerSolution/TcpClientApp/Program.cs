56a849d8dab1ff9008592ae1bc716e83e9791a2a
SunnyB98 <113575525+SunnyB98@users.noreply.github.com>
2025-05-23 14:37:32 +0530

    Update Program.cs

--- MS.net/TcpMessengerSolution/TcpClientApp/Program.cs
+++ MS.net/TcpMessengerSolution/TcpClientApp/Program.cs
@@ -2,31 +2,36 @@ using System;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
+using Serilog;
 
 class TcpClientApp
 {
     static void Main()
     {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("Logs/client_log.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        Log.Information("Connecting to server...");
+
         TcpClient client = new TcpClient("127.0.0.1", 5000);
-        Console.WriteLine("Connected to TCP Server.");
+        Log.Information("Connected to server.");
 
         NetworkStream stream = client.GetStream();
         StreamReader reader = new StreamReader(stream, Encoding.UTF8);
         StreamWriter writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
 
-        using (StreamWriter log = new StreamWriter("client_chatlog.txt", true))
+        while (true)
         {
-            while (true)
-            {
-                Console.Write("You: ");
-                string message = Console.ReadLine();
-                writer.WriteLine(message);
-                log.WriteLine($"Client: {message}");
+            Console.Write("You: ");
+            string message = Console.ReadLine();
+            writer.WriteLine(message);
+            Log.Information("Client: {Message}", message);
 
-                string serverMessage = reader.ReadLine();
-                Console.WriteLine("Server: " + serverMessage);
-                log.WriteLine($"Server: {serverMessage}");
-            }
+            string serverMessage = reader.ReadLine();
+            Log.Information("Server: {ServerMessage}", serverMessage);
+            Console.WriteLine("Server: " + serverMessage);
         }
 
         client.Close();
